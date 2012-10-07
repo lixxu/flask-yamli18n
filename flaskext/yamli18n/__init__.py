4dbe0eade3906bf471e4f0ea7f6924e019053ece
Lix Xu <xuzenglin@gmail.com>
2012-10-07 23:33:57 +0800

    first commit

---
+++ flaskext/yamli18n/__init__.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+'''
+    flaskext.yamli18n
+    ~~~~~~~~~~~~~~~~~
+
+    Use yaml files for i18n support in Flask framework.
+
+    :copyright: (c) 2012 by Lix Xu.
+    :license: BSD, see LICENSE for more details.
+
+'''
+
+import os
+import os.path
+from collections import defaultdict
+
+from yaml import load
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
+from flask import session, request
+
+
+class YAMLI18N(object):
+    def __init__(self, app=None, reload=False):
+        self.ymls = defaultdict(dict)
+        self.ts = {}
+        self.reload = reload
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        locales_folder = app.config.get('YAML_LOCALE_PATH', 'locales')
+        self.locales_path = os.path.join(app.root_path, locales_folder)
+        self.load_ymls()
+
+    def load_ymls(self):
+        '''yaml files structure:
+            YAML_LOCALE_PATH\
+                blueprint1\
+                    en.yml
+                    ...
+                blueprint2\
+                    en.yml
+                    ...
+        '''
+        if self.ymls and self.reload is False:
+            return
+
+        for d in os.listdir(self.locales_path):
+            if d.startswith('.'):
+                continue
+
+            d_path = os.path.join(self.locales_path, d)
+            if os.path.isfile(d_path):
+                continue
+
+            for yml_file in os.listdir(d_path):
+                if yml_file.endswith(('.yml', '.yaml')):
+                    file_path = os.path.join(d_path, yml_file)
+                    if self.reload is True:
+                        mt = os.path.getmtime(file_path)
+                        if self.ts.get(file_path, None) == mt:
+                            continue
+
+                    lang = os.path.splitext(yml_file)[0].lower()
+                    with open(file_path) as f:
+                        self.ymls[d][lang] = load(f, Loader=Loader)
+
+                    if self.reload is True:
+                        self.ts[file_path] = mt
+
+    def t(self, text, lang=None, failback='en', **kwargs):
+        '''text:
+            1. name = default -> lang -> name
+            2. .name = blueprint -> lang -> name
+            3. ..name = blueprint -> lang -> endpoint -> name
+            4. users.name = users_blueprint -> lang -> name
+            5. users.edit.login = users_blueprint -> lang
+                                  -> edit_endpoint -> name
+
+        '''
+        self.load_ymls()
+        if lang is None:
+            lang = session.get('lang', 'en')
+
+        if not text:
+            return text
+
+        # is format 1
+        if '.' not in text or (text[-1] == '.' and text.count('.') == 1):
+            if lang not in self.ymls['default']:
+                lang = failback
+
+            if lang not in self.ymls['default']:
+                return text
+
+            msg = self.ymls['default'][lang].get(text, text)
+            return msg.format(**kwargs) if kwargs else msg
+
+        endpoint = None
+        if text.startswith('..'):  # is format 3
+            blueprint = request.blueprint
+            endpoint = '.{0}'.format(request.endpoint.split('.')[-1])
+            s = text[2:]
+        elif text.startswith('.'):  # is format 2
+            blueprint = request.blueprint
+            s = text[1:]
+        elif text.count('.') == 1:  # is format 4
+            blueprint, s = text.split('.')
+        else:  # is format 5
+            blueprint, endpoint, s = text.split('.')
+            endpoint = '.' + endpoint
+
+        if blueprint not in self.ymls:
+            return text.format(**kwargs) if kwargs else text
+
+        bp = self.ymls[blueprint]
+        if lang not in bp:
+            lang = failback
+
+        if lang not in bp:
+            return text
+
+        yml = bp[lang]
+        if endpoint in yml:
+            msg = yml[endpoint].get(s, text)
+            return msg.format(**kwargs) if kwargs else msg
+
+        if kwargs:
+            return yml.get(s, text).format(**kwargs)
+
+        return yml.get(s, text)
---
+++ setup.py
@@ -0,0 +1,37 @@
+"""
+Flask-yamli18n
+--------------
+
+Use YAML files for i18n in Flask
+It requires Python2.6+ as it uses string.format syntax.
+Or you can modify it to python2.5 '%s' syntax.
+"""
+from setuptools import setup
+
+setup(
+    name='flask-yamli18n',
+    version='0.1',
+    url='https://github.com/lixxu/flask-yamli18n',
+    license='BSD',
+    author='Lix Xu',
+    author_email='xuzenglin@gmail.com',
+    description='Use yaml files as translation files in flask',
+    long_description=__doc__,
+    namespace_packages=['flaskext'],
+    packages=['flaskext', 'flaskext.yamli18n'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'PyYAML'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
